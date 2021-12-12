4b6ff2ee44cb62850caf437bdd08b63c07e4c343
countlesszx <95583936+countlesszx@users.noreply.github.com>
2021-12-12 10:35:54 -0800

    Add files via upload

---
+++ story_time.js
@@ -0,0 +1,36 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to the Demon Slayer Corps!";
+let name = 'Kyojuro Rengoku';
+let title = 'Flame Hashira';
+let worldName = 'Demon Slayer Corps';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 21;
+let year = 1920;
+let century = 20;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsUsed = ['Nichirin Katana', 'Flame Breathing', 'Bento'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavBreathing = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is " + name + ".");
+console.log("I'm the proud " + title + ", surpassing my father.");
+console.log("As a Demon Slayer of the " + worldName + ", my duty is to destroy all living demons in this world.");
+console.log("Currently it's " + year + " in Taisho Era." + "\n");
+console.log("Does anyone of you younglings have a question?");
+console.log("Me: How do you become a Demon Slayer?");
+console.log("Kyojuro: You must undergo a challenging test called the Slayer Exams, but for starters, you must aquire breathing technique first.");
+console.log("Me: What breathing style do you have Rengoku-San?");
+console.log("Kyojuro: " + hasFavBreathing + ", my breathing style is known as " + weaponsUsed[1] + ".");
+console.log("But in order for you to obtain a breathing, you must find a worthy trainer of your desired breathing style." + "\n");
+console.log("I must also mention that demons are weak to the sun, and they are abnormally strong, the only thing that can slay a demon is a " + weaponsUsed[0] + ", since Nichirins are made out of sun ores, which are ores absorbed by the sun.");
+console.log("Me: Thank you so much Rengoku-San!" + "\n");
+console.log("Kyojuro: Young Ones! Be sure to find yourself a breathing trainer and slay all existing demons in this world!");
